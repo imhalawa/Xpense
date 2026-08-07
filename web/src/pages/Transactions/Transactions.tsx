c9a619972ae96c1e883a1c627da7f157db96d09c
Mohamed Halawa <imhalawa@outlook.com>
2026-08-08 01:00:48 +0200

    refactor: collapse three navigation bars into one and mount the notification bell

--- web/src/pages/Transactions/Transactions.tsx
+++ web/src/pages/Transactions/Transactions.tsx
@@ -1,13 +1,24 @@
 import { Box, Grid } from "@mui/material";
 import TransactionsGrid from "./TransactionsGrid/TransactionsGrid";
+import TransactionsForm from "./TransactionsForm/TransactionsForm";
+import Calendar from "../../components/Calendary/Calendar";
 import Page from "../../components/Page/Page";
+import { useCalendar } from "../../contexts/CalendarContext";
 
 const Transactions = () => {
+  const { selectedDate } = useCalendar();
+
   return (
     <Page title="Transactions" headerColor={"primary.dark"} headerBackgroundColor={"white"}>
-      <Grid size={12}>
+      <Grid size={{ xs: 12, md: 8 }}>
         <Box sx={{ height: "calc(100vh - 256px)", width: "100%" }}>
           <TransactionsGrid size={10} />
+        </Box>
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <TransactionsForm selectedDate={selectedDate} />
+          <Calendar />
         </Box>
       </Grid>
     </Page>
