d8fbc83b0b4e865b7a859f5102bed134b2deda49
Mohamed Halawa <imhalawa@outlook.com>
2026-08-08 03:49:14 +0200

    feat: add fluent root color scheme hook

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,8 +6,9 @@ import Transactions from "./pages/Transactions/Transactions.tsx";
 import Budgets from "./pages/Budgets/Budgets.tsx";
 import Settings from "./pages/Settings/Settings.tsx";
 import Layout from "./pages/Layout.tsx";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme/theme";
+import { FluentProvider } from "@fluentui/react-components";
+import { darkTheme, lightTheme } from "./fluent/theme.ts";
+import { useColorScheme } from "./fluent/useColorScheme.ts";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LoadingContextProvider } from "./contexts/LoadingContext.tsx";
@@ -17,26 +18,27 @@ import axios from "axios";
 function App() {
   axios.defaults.baseURL = "http://localhost:4000/";
 
+  const { resolved } = useColorScheme();
+
   return (
-    <>
-      <CssBaseline />
-      <ThemeProvider theme={theme}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <LoadingContextProvider>
-            <TransactionUtilitiesContextProvider>
-              <Routes>
-                <Route path="/" element={<Layout />}>
-                  <Route index element={<Overview />} />
-                  <Route path="/transactions" element={<Transactions />} />
-                  <Route path="/budgets" element={<Budgets />} />
-                  <Route path="/settings" element={<Settings />} />
-                </Route>
-              </Routes>
-            </TransactionUtilitiesContextProvider>
-          </LoadingContextProvider>
-        </LocalizationProvider>
-      </ThemeProvider>
-    </>
+    <FluentProvider theme={resolved === "dark" ? darkTheme : lightTheme}>
+      <div style={{ minHeight: "100vh" }}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LoadingContextProvider>
+          <TransactionUtilitiesContextProvider>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Overview />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/budgets" element={<Budgets />} />
+                <Route path="/settings" element={<Settings />} />
+              </Route>
+            </Routes>
+          </TransactionUtilitiesContextProvider>
+        </LoadingContextProvider>
+      </LocalizationProvider>
+      </div>
+    </FluentProvider>
   );
 }
 
